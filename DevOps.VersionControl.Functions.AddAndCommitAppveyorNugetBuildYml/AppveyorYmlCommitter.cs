294ceccf5b205f33db2611404960eab49c496cc2
Christopher Dorst <chris@dorst.email>
2018-01-14 17:32:29 -0600

    Add AppveyorYmlCommitter

---
+++ DevOps.VersionControl.Functions.AddAndCommitAppveyorNugetBuildYml/AppveyorYmlCommitter.cs
@@ -0,0 +1,17 @@
+﻿using DevOps.VersionControl.Structures.GitCommitUser;
+using static DevOps.VersionControl.Functions.AddAndCommitGitChanges.GitChangeAdderAndCommitter;
+using static Metaproject.Cdn.Containers.Build.Helpers.BuildContentHelper;
+
+namespace DevOps.VersionControl.Functions.AddAndCommitAppveyorNugetBuildYml
+{
+    public static class AppveyorYmlCommitter
+    {
+        private const string CommitMessage = "Add appveyor.yml";
+
+        public static void AddAndCommitLicense(string repositoryDirectory, UserInfo? user = null)
+        {
+            SaveAppveyorYml(repositoryDirectory);
+            AddAndCommitChanges(repositoryDirectory, CommitMessage, user);
+        }
+    }
+}
